a510ec2cff0e1069aa618dd08b688927e82cf7ee
unknown <emmateye42@gmail.com>
2021-11-08 16:18:46 +0000

    contact page added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,6 +7,13 @@ const  About = () =>(
             <div className="about__hero">
                 <h3>About Us:</h3>
             </div>
+          <div className="about__content">
+              <p>The "Paymyrent" App/Website is an initiative of
+                  KOS group powered by Jaskos Data. Our whole idea 
+                  is to help and solve the payment of rent to qualified
+                   individual who cannot afford to pay rents in bulk.
+              </p>
+          </div>
         </div>
     );
 
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,6 +6,31 @@ const  Contact = () =>{
             <div className="contact__hero">
                 <h3>Contact Us:</h3>
             </div>
+            <div className="contact__content">
+                <div className="contact__content__name">
+                    <div>
+                        <p>First Name*</p>
+                        <input type="text" />
+                    </div>
+                    <div>
+                        <p>Last Name*</p>
+                        <input type="text" />
+                    </div>
+                </div>
+                <div className="contact__email">
+                    <p>Email*</p>
+                    <input type="email" />
+                </div>
+                <div className="contact__email">
+                    <p>Subject*</p>
+                    <input type="email" />
+                </div>
+               <div className="contact__message">
+                   <p>Message*</p>
+                   <textarea rows="4"></textarea>
+               </div>
+               <button>Submit</button>
+            </div>
         </div>
     )
 }
